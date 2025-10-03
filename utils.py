0811677494db765a44d64cd967fb620456766eee
Jun Aishima <jaishima@bnl.gov>
2025-10-03 17:11:45 -0400

    make new clients each time get_tiled_client() is called
     * the underlying object is not async-compatible so make a new client
       every time one is requested instead of sharing

--- utils.py
+++ utils.py
@@ -5,9 +5,8 @@ import os
 
 LOCATION="tst"
 
-os.environ["TILED_API_KEY"] = Secret.load(f"tiled-{LOCATION}-api-key").get()
-tiled_client = from_profile("nsls2")[LOCATION]["raw"]
-os.environ.pop("TILED_API_KEY")
-
 def get_tiled_client():
+    os.environ["TILED_API_KEY"] = Secret.load(f"tiled-{LOCATION}-api-key").get()
+    tiled_client = from_profile("nsls2")[LOCATION]["raw"]
+    os.environ.pop("TILED_API_KEY")
     return tiled_client
