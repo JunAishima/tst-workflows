7a40d29250abc9258755261ecec04fc630e788b5
Jun Aishima <jaishima@bnl.gov>
2025-10-07 10:57:46 -0400

    fix bad import

--- data_validation.py
+++ data_validation.py
@@ -1,14 +1,14 @@
 from prefect import task, flow, get_run_logger
 from prefect.blocks.system import Secret
 import time as ttime
-from tiled.client import get_profile
+from tiled.client import from_profile
 import os
 
 @task(retries=2, retry_delay_seconds=10)
 def read_all_streams(uid, beamline_acronym):
     logger = get_run_logger()
     api_key = Secret.load("tiled-tst-api-key").get()
-    cl = get_profile("nsls2", api_key=api_key)
+    cl = from_profile("nsls2", api_key=api_key)
     run = cl["tst"]["raw"][uid]
     logger.info(f"Validating uid {run.start['uid']}")
     start_time = ttime.monotonic()
