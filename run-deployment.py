772daf3c41e26dbb3eb61f8fdaef9b9101e432c4
Jun Aishima <jaishima@bnl.gov>
2025-10-08 10:28:33 -0400

    give a dummy uid

--- run-deployment.py
+++ run-deployment.py
@@ -5,6 +5,6 @@ from datetime import datetime
 
 run_deployment(
     name="end-of-run-workflow/tst-end-of-run-workflow-docker",
-    parameters={"stop_doc": {"run_start": ""}},
+    parameters={"stop_doc": {"run_start": "adfwerwerwerw"}},
     timeout=15,  # don't wait for the run to finish # edit to 15 sec
 )
