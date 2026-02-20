86f12632e0f3019c2005aa603fced60fde70efb4
Jun Aishima <jaishima@bnl.gov>
2026-02-20 14:50:55 -0500

    fix pre-commit issues

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -5,7 +5,7 @@ from test_extra_client import get_other_docs
 
 
 @task
-def log_completion(dry_run=dry_run):
+def log_completion(dry_run=False):
     logger = get_run_logger()
     logger.info(f"Complete! dry_run:{dry_run}")
 
