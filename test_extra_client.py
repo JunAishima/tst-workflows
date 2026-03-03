2feb28bd5bbdef0046e099e960943f47838384f3
Jun Aishima <jaishima@bnl.gov>
2026-03-03 16:51:40 -0500

    fix Tiled API key handling for utils/test_extra_client

--- test_extra_client.py
+++ test_extra_client.py
@@ -3,7 +3,7 @@ from utils import get_tiled_client
 
 
 @task
-def get_other_docs(uid):
+def get_other_docs(uid, api_key=api_key):
     logger = get_run_logger()
     result = get_tiled_client()["raw"][uid]
     for name, doc in result.documents():
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-from dotenv import load_dotenv
 from tiled.client import from_profile
 
 import os
@@ -6,9 +5,6 @@ import os
 LOCATION = "tst"
 
 
-def get_tiled_client():
-    with open("/srv/tiled.secret", "r") as secrets:
-        load_dotenv(stream=secrets)
-    api_key = os.environ["TILED_API_KEY"]
+def get_tiled_client(api_key=None):
     tiled_client = from_profile("nsls2", api_key=api_key)[LOCATION]
     return tiled_client
