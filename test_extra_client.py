4ffc6595d11ee6903d90b325d76ba1f26d542020
Jun Aishima <jaishima@bnl.gov>
2026-03-09 11:14:29 -0400

    fix pre-commit issue

--- test_extra_client.py
+++ test_extra_client.py
@@ -6,7 +6,7 @@ from utils import get_tiled_client
 def get_other_docs(uid, api_key=None, dry_run=False):
     logger = get_run_logger()
     if dry_run:
-        logger.info(f"Dry run: not getting other docs")
+        logger.info("Dry run: not getting other docs")
         return
     result = get_tiled_client()["raw"][uid]
     for name, doc in result.documents():
