fb0914e5ac9727c6d32b9b7c1437a949ee756a5d
Jun Aishima <jaishima@bnl.gov>
2026-03-12 11:26:27 -0400

    rename env var used for test Tiled API key

--- test_run.py
+++ test_run.py
@@ -13,5 +13,8 @@ def prefect_disable_logging():
 
 def test_end_of_run_workflow(prefect_disable_logging):
     print("starting test!")
-    assert end_of_run_workflow(stop_doc={"run_start":"f0954c84-f652-4350-9f6d-44b724f4ed9f"}, api_key=os.environ["TILED_API_KEY"])
+    assert end_of_run_workflow(
+        stop_doc={"run_start": "f0954c84-f652-4350-9f6d-44b724f4ed9f"},
+        api_key=os.environ["TEST_TILED_API_KEY"],
+    )
     print("finished test!")
