6cb188181187c1b5954eff2cc8b23d30bc530400
Jun Aishima <jaishima@bnl.gov>
2026-05-11 17:36:53 -0400

    add message with link also if successful for testing

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -56,6 +56,11 @@ def slack(func):
             mon_prefect_tst.notify(
                 f":white_check_mark: {CATALOG_NAME} flow-run successful. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}```"
             )
+            flow_run = FlowRunContext.get().flow_run
+            group_message = f":bangbang: {CATALOG_NAME} flow-run failed. <https://{PREFECT_UI_URL.value()}/flow-runs/"
+                            f"flow-run/{flow_run.id}|the flow run link> (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```{tb[-1]}```"
+            mon_prefect_tst.notify(group_message)
+
             return result
         except Exception as e:
             tb = traceback.format_exception_only(e)
